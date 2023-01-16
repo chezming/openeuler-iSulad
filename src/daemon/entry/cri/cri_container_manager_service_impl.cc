d883924323f18140ae81aa4ff063218b9b94d25f
ilya.kuksenok <ilya.kuksenok@huawei.com>
2023-01-16 14:26:55 +0300

    add container memory stats

--- src/daemon/entry/cri/cri_container_manager_service_impl.cc
+++ src/daemon/entry/cri/cri_container_manager_service_impl.cc
@@ -835,6 +835,26 @@ void ContainerManagerServiceImpl::ContainerStatsToGRPC(
             container->mutable_memory()->set_timestamp(timestamp);
         }
 
+        if (response->container_stats[i]->avaliable_bytes != 0){
+            uint64_t workingset = response->container_stats[i]->avaliable_bytes;
+            container->mutable_memory()->mutable_available_bytes()->set_value(workingset);
+        }
+        if (response->container_stats[i]->usage_bytes != 0){
+            uint64_t workingset = response->container_stats[i]->usage_bytes;
+            container->mutable_memory()->mutable_usage_bytes()->set_value(workingset);
+        }
+        if (response->container_stats[i]->rss_bytes != 0){
+            uint64_t workingset = response->container_stats[i]->rss_bytes;
+            container->mutable_memory()->mutable_rss_bytes()->set_value(workingset);
+        }
+        if (response->container_stats[i]->page_faults != 0){
+            uint64_t workingset = response->container_stats[i]->page_faults;
+            container->mutable_memory()->mutable_page_faults()->set_value(workingset);
+        }
+        if (response->container_stats[i]->major_page_faults != 0){
+            uint64_t workingset = response->container_stats[i]->major_page_faults;
+            container->mutable_memory()->mutable_major_page_faults()->set_value(workingset);
+        }
         if (response->container_stats[i]->cpu_use_nanos != 0u) {
             container->mutable_cpu()->mutable_usage_core_nano_seconds()->set_value(
                 response->container_stats[i]->cpu_use_nanos);
